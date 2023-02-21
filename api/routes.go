8013710402a5aa80338974ecc35c84eaa0b0db19
onao13 <khovratovic15@yandex.ru>
2023-02-21 11:42:19 +0300

    Added support category controller and delete help path api

--- api/routes.go
+++ api/routes.go
@@ -3,29 +3,28 @@ package api
 import (
 	"github.com/gorilla/mux"
 	"severyanochka/internals/app/controller/public"
-	"severyanochka/internals/app/controller/public/help"
 )
 
 const (
 	restApiPath = "/api/v1/"
-	helpApiPath = "/help/"
 )
 
 // CreateRoute Пути, по которым доступен севрер
 func CreateRoute(
 	productController *public.ProductController,
-	helpController *help.HelpController,
 	mainPageController *public.MainPageController,
 	articleController *public.ArticleController,
 	specialOfferController *public.SpecialOffersController,
+	categoryController *public.CategoryController,
 ) *mux.Router {
 	route := mux.NewRouter()
 	//REST API
-	route.HandleFunc(restApiPath+"page/preview-main", mainPageController.PreviewMainPage).Methods("GET")
+	route.HandleFunc(restApiPath+"page/home", mainPageController.PreviewMainPage).Methods("GET")
 	route.HandleFunc(restApiPath+"products/search", productController.SearchByName).Methods("GET").Queries("q", "{q}")
+	route.HandleFunc(restApiPath+"products/get/{id:[0-9]+}", productController.FindById).Methods("GET")
 	route.HandleFunc(restApiPath+"articles/search/{id:[0-9]+}", articleController.FindById).Methods("GET")
 	route.HandleFunc(restApiPath+"offers/search/{id:[0-9]+}", specialOfferController.FindById).Methods("GET")
-	//HTML DOC API
-	route.HandleFunc(helpApiPath+"main-page", helpController.ShowMainPageDoc)
+	route.HandleFunc(restApiPath+"categories", categoryController.GetAll).Methods("GET")
+	route.HandleFunc(restApiPath+"categories/{id:[0-9]+}/products", categoryController.FindCategoryProductsById).Methods("GET")
 	return route
 }
