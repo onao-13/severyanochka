dac55438219a234206e9129b8418e0fdb80fb44a
onao13 <khovratovic15@yandex.ru>
2023-02-21 11:34:36 +0300

    Add JSON field

--- internals/app/entity/product/categories.go
+++ internals/app/entity/product/categories.go
@@ -2,7 +2,7 @@ package product
 
 // Categories Таблица категории product.Products
 type Categories struct {
-	Id       int64
-	Name     string
-	ImageUrl string
+	Id       int64  `json:"id"`
+	Name     string `json:"name"`
+	ImageUrl string `json:"imageUrl"`
 }
